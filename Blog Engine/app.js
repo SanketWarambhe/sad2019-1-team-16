f86172cb85364bfdf7898f00dc6d59b4efb7bb2f
DESKTOP-HGPF4DU\Sanket <sanketwarambhe@gmail.com>
2019-01-31 20:46:02 +0100

    Refactoring code. (Adding the article routes inside the routes folder)

--- Blog Engine/app.js
+++ Blog Engine/app.js
@@ -2,52 +2,25 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const bodyParser = require('body-parser');
-const multer = require('multer');
+
 const expressValidator = require('express-validator');
-const flash = require('connect-flash');
+
 const session = require('express-session');
 var nodemailer = require('nodemailer');
 const passport =require('passport');
 const config = require('./config/database');
 const ITEMS_PER_PAGE = 3;
 
-//Strategy for storing images
-const storage = multer.diskStorage({
-    destination: function(req, res, cb) {
-        cb(null, './uploads/');
-    },
-    filename: function(req, file, cb) {
-        cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
-    }
-});
-
-const fileFilter = (req, file, cb) => {
-    //rejecting a file
-    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
-        cb(null, true);
-    }
-    else {
-        cb(null, false);
-    }
-}
-const upload = multer({
-    storage: storage,
-    limit: {
-        fileSize: 1024 * 1024 * 5 //max 5 mb image upload
-    },
-    fileFilter: fileFilter
-});
-
 mongoose.connect(config.database, { useNewUrlParser: true });
 let db = mongoose.connection;
 
 //Check for successful connections
-db.once('open', function() {
+db.once('open', function () {
     console.log('Connected to MongoDB');
 });
 
 //Checking for errors;
-db.on('error', function(err) {
+db.on('error', function (err) {
     console.log(err);
 });
 
@@ -126,121 +99,9 @@ app.get('/', (req, res, next) => {
         }
     });
 });
-app.get('/article', (req, res,next) => {
-    res.render('add-article', {
-       title:'Add Article',
-       errors: null
-    });
-    console.log('add page');
-});
 
-//get single article
-app.get('/article/:id', function (req, res){
-    Article.findById(req.params.id, function(err, article){
-        res.render('view-article', {
-            title: 'View Article',
-            article: article
-        });
-    });
-});
 
-//Adding Comments middleware
-app.post('/article/:id', function (req, res) {
-    Article.findById(req.params.id, function (err, article) {
-        article.comment.push(req.body.comment);
-        let query = { _id: req.params.id }
-        Article.update(query, article, function (err) {
-            if (err) {
-                console.log(err);
-                return;
-            } else {
-                res.render('view-article', {
-                    title: 'View Article',
-                    article: article
-                });
-            }
-        });
-    });
-});
 
-//POST route for submiting articles
-app.post('/article', upload.single('articleImage'), (req, res, next) => {
-    console.log(req.file);
-
-    req.checkBody('title', 'Title is required').notEmpty();
-    req.checkBody('author', 'Author is required').notEmpty();
-    req.checkBody('body', 'Article body is required').notEmpty();
-    
-    // Getting all validation errors
-    let errors = req.validationErrors();
-
-    if(errors){
-        res.render('add-article', {
-            title: 'Add-Article',
-            errors: errors
-        });
-    } else {
-        let article = new Article();
-        article.title = req.body.title;
-        article.author = req.body.author;
-        article.body = req.body.body;
-        article.articleImage = req.file.filename;
-        article.save(err => {
-            if(err){
-                console.log(err);
-                return;
-            } else {
-                req.flash('success', 'Article Added Successfully!');
-                res.redirect('/');
-        }
-    });
-    }
-});
-
-//Loading edit form
-app.get('/article/edit/:id', function (req, res){
-    Article.findById(req.params.id, function(err, article){
-        res.render('edit-article', {
-            title: 'Edit Article',
-            article: article
-        });
-    });
-});
-
-//Update Submit POST Route
-app.post('/article/edit/:id', upload.single('articleImage'), (req, res, next) => {
-    console.log(req.file);
-    let article = {};
-    article.title = req.body.title;
-    article.author = req.body.author;
-    article.body = req.body.body;
-    if(req.file != undefined || req.file != null){
-        article.articleImage = req.file.filename;
-    }
-    let query = {_id:req.params.id}
-    Article.update(query, article,function(err) {
-        if(err){
-            console.log(err);
-            return;
-        } else {
-            req.flash('success', 'Article Updated Successfully!');
-            res.redirect('/');
-        }
-    });
-});
-
-//Deleting ROUTE
-
-app.delete('/article/:id', function(req, res){
-    let query = {_id:req.params.id}
-
-    Article.remove(query, function(err){
-        if(err){
-            console.log(err);
-        }
-        res.send('Success');
-    });
-});
 //edit profile route
 app.get('/edit-profile', (req, res) => {
     res.render('edit-profile', {
@@ -256,7 +117,7 @@ app.get('/profile',(req, res) => {
 //contact us route
 app.get('/contact-us',(req,res) => {
     res.render('contact-us', {
-        title:'contact us page',
+        title:'Contact Us',
         msg: 'Enter your details below'
     });
 });
@@ -310,7 +171,10 @@ app.post('/contact-us', (req,res)=> {
 
 //Route files
 let users = require('./routes/users');
+let articles = require('./routes/articles');
 app.use('/users',users);
+app.use('/articles', articles);
+
 //about us route
 app.get('/about-us',(req,res)=>{
     res.render('about-us',{
--- Blog Engine/routes/articles.js
+++ Blog Engine/routes/articles.js
@@ -1,3 +1,155 @@
 const express = require('express');
 const router = express.Router();
 
+const flash = require('connect-flash');
+const multer = require('multer');
+
+//Getting out models
+let Article = require('../models/articles');
+
+//Strategy for storing images
+const storage = multer.diskStorage({
+    destination: function (req, res, cb) {
+        cb(null, './uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    //rejecting a file
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb(null, true);
+    }
+    else {
+        cb(null, false);
+    }
+}
+const upload = multer({
+    storage: storage,
+    limit: {
+        fileSize: 1024 * 1024 * 5 //max 5 mb image upload
+    },
+    fileFilter: fileFilter
+});
+
+router.get('/', (req, res, next) => {
+    res.render('add-article', {
+        title: 'Add Article',
+        errors: null
+    });
+    console.log('add page');
+});
+
+//POST route for submiting articles
+router.post('/', upload.single('articleImage'), (req, res, next) => {
+    console.log(req.file);
+
+    req.checkBody('title', 'Title is required').notEmpty();
+    req.checkBody('author', 'Author is required').notEmpty();
+    req.checkBody('body', 'Article body is required').notEmpty();
+
+    // Getting all validation errors
+    let errors = req.validationErrors();
+
+    if (errors) {
+        res.render('add-article', {
+            title: 'Add-Article',
+            errors: errors
+        });
+    } else {
+        let article = new Article();
+        article.title = req.body.title;
+        article.author = req.body.author;
+        article.body = req.body.body;
+        article.articleImage = req.file.filename;
+        article.save(err => {
+            if (err) {
+                console.log(err);
+                return;
+            } else {
+                req.flash('success', 'Article Added Successfully!');
+                res.redirect('/');
+            }
+        });
+    }
+});
+
+//get single article
+router.get('/:id', function (req, res) {
+    Article.findById(req.params.id, function (err, article) {
+        res.render('view-article', {
+            title: 'View Article',
+            article: article
+        });
+    });
+});
+
+//Adding Comments middleware
+router.post('/:id', function (req, res) {
+    Article.findById(req.params.id, function (err, article) {
+        article.comment.push(req.body.comment);
+        let query = { _id: req.params.id }
+        Article.update(query, article, function (err) {
+            if (err) {
+                console.log(err);
+                return;
+            } else {
+                res.render('view-article', {
+                    title: 'View Article',
+                    article: article
+                });
+            }
+        });
+    });
+});
+
+
+
+//Loading edit form
+router.get('/edit/:id', function (req, res) {
+    Article.findById(req.params.id, function (err, article) {
+        res.render('edit-article', {
+            title: 'Edit Article',
+            article: article
+        });
+    });
+});
+
+//Update Submit POST Route
+router.post('/edit/:id', upload.single('articleImage'), (req, res, next) => {
+    console.log(req.file);
+    let article = {};
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+    if (req.file != undefined || req.file != null) {
+        article.articleImage = req.file.filename;
+    }
+    let query = { _id: req.params.id }
+    Article.update(query, article, function (err) {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            req.flash('success', 'Article Updated Successfully!');
+            res.redirect('/');
+        }
+    });
+});
+
+//Deleting ROUTE
+
+router.delete('/:id', function (req, res) {
+    let query = { _id: req.params.id }
+
+    Article.remove(query, function (err) {
+        if (err) {
+            console.log(err);
+        }
+        res.send('Success');
+    });
+});
+
+module.exports = router;
