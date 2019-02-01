9ee3bfdd640744e9e1a687b2b2579510e3c7782f
DESKTOP-HGPF4DU\Sanket <sanketwarambhe@gmail.com>
2019-02-01 13:20:52 +0100

    Code for showing profile image preview while registering a new user

--- Blog Engine/public/js/main.js
+++ Blog Engine/public/js/main.js
@@ -15,3 +15,15 @@ $(document).ready(function(){
       });
     });
   });
+
+function showImage(){
+  if(this.files && this.files[0]){
+    var obj = new FileReader();
+    obj.onload = function(data){
+      var image = document.getElementById('profilePreview');
+      image.src = data.target.result;
+      image.style.display = "block";
+    }
+    obj.readAsDataURL(this.files[0]);
+  }
+}
