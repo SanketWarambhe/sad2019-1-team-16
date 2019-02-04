6f8c4ffc753bfb465d8d6303a633ef62b984a33f
DESKTOP-HGPF4DU\Sanket <sanketwarambhe@gmail.com>
2019-02-04 15:26:29 +0100

    Changes for styling the profile page

--- Blog Engine/routes/users.js
+++ Blog Engine/routes/users.js
@@ -181,7 +181,6 @@ router.post('/edit/:id', (req, res) => {
         const email = req.body.email;
         const username = req.body.username;
         const password = req.body.password;
-        const password2 = req.body.password2;
 
         req.checkBody('name', 'Name is required').notEmpty();
         req.checkBody('email', 'Email is required').notEmpty();
